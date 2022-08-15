55fe00eb498095718c25776e82c75f88b6cfff55
tsj0953 <tsj0953@naver.com>
2022-08-15 20:14:30 +0900

    search

---
+++ js/search.js
@@ -0,0 +1,10 @@
+function KindOfHome() {
+    var click= document.getElementById('kindofhome');
+
+    if(click.style.display === "none"){
+        click.style.display = "block";
+
+    }else{
+        click.style.display = "none";                 
+    }
+}
