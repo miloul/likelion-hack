3a8d4677ce96d9b2662f90eb647160f4af92b2dc
milou <milouw56@gamil.com>
2022-08-17 17:37:14 +0900

    hosting front-end

--- js/hosting.js
+++ js/hosting.js
@@ -1,5 +1,21 @@
 function KindOfHome() {
     var click= document.getElementById('kindofhome');
+    
+    if(click.style.display === "none"){
+        click.style.display = "block";
+
+    }else{
+        click.style.display = "none";                 
+    }
+}
+
+document.getElementById('kindofhome').addEventListener('change', function() {
+    console.log('You selected: ', this.value);
+});
+
+
+function InputDays() {
+    var click= document.getElementById('inputdays');
 
     if(click.style.display === "none"){
         click.style.display = "block";
@@ -9,8 +25,8 @@ function KindOfHome() {
     }
 }
 
-function Location() {
-    var click= document.getElementById('location');
+function InputInfo() {
+    var click= document.getElementById('inputinfo');
 
     if(click.style.display === "none"){
         click.style.display = "block";
@@ -20,14 +36,34 @@ function Location() {
     }
 }
 
-function loadFile(event) {
-    var reader = new FileReader();
+function readInputFile(e){
+    var sel_files = [];
+    
+    sel_files = [];
 
-    reader.onload = function(event) {
-      var img = document.createElement("img");
-      img.setAttribute("src", event.target.result);
-      document.querySelector("div#image_container").appendChild(img);
-    };
-
-    reader.readAsDataURL(event.target.files[0]);
+    var files = e.target.files;
+    var fileArr = Array.prototype.slice.call(files);
+    var index = 0;
+    
+    fileArr.forEach(function(f){
+    	if(!f.type.match("image/.*")){
+        	alert("이미지 확장자만 업로드 가능합니다.");
+            return;
+        };
+        if(files.length < 11){
+        	sel_files.push(f);
+            var reader = new FileReader();
+            reader.onload = function(e){
+                var img = document.createElement("img");
+                img.setAttribute("src", e.target.result);
+                img.style.width = "30%";
+                document.querySelector("div#image").appendChild(img);
+                index++;
+            };
+            reader.readAsDataURL(f);
+        }
+    })
+    if(files.length > 4){
+    	alert("최대 10장까지 업로드 할 수 있습니다.");
+    }
 }
