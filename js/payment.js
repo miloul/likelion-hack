74a78ffcc34da05b3d9b02b9c3adab28766e7c3c
tsj0953 <tsj0953@naver.com>
2022-08-18 04:50:42 +0900

    payment making ..

---
+++ js/payment.js
@@ -0,0 +1,14 @@
+function count(type){
+    const resultElement = document.getElementById('result');
+
+    let number = resultElement.innerText;
+
+    if(type === 'plus'){
+        number = parseInt(number) + 1;
+    }
+    else if(type === 'minus') {
+        number = parseInt(number) - 1;
+    }
+
+    resultElement.innerText = number;
+}
